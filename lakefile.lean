b083bece8beb6effaacd7a799a75fa0cd0c133d2
serhatemrecoban <serhat.coban@epfl.ch>
2026-05-24 17:10:32 +0200

    Add checkdecls dependency for blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -3,5 +3,7 @@ open Lake DSL
 
 package «PerfectFunctionalRepresentations»
 
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
 lean_lib «PerfectFunctionalRepresentations» where
   globs := #[Glob.submodules `PerfectFunctionalRepresentations]
